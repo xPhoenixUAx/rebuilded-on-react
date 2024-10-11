bd42cbdad924824fd5a886b9f9b3112e3e8da7a4
xPhoenixUAx <pavloff12@gmail.com>
2024-10-11 19:41:42 +0300

    upd

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import s from "./Hero.module.css";
 const Hero = () => {
   return (
     <div>
-      <section className="heroSection">
+      <section className={s.heroSection}>
         <div className={s.container}>
           <h1 className={s.heroSectionTitle}>
             Effective Solutions for Your Business
--- src/components/What/What.jsx
+++ src/components/What/What.jsx
@@ -1,7 +1,65 @@
-import React from "react";
-
+import s from "./What.module.css";
+import icons from "../images/images/icons.svg";
 const What = () => {
-  return <div></div>;
+  return (
+    <div>
+      <section className={s.advantagesSection}>
+        <div className={s.container}>
+          <h2 className={s.visuallyHidden}>Our Advantages</h2>
+          <ul className={s.advantagesBox}>
+            <li className={s.advantagesPunkts}>
+              <div className={s.iconBox}>
+                <svg className={s.advIcon} width="64" height="64">
+                  <use href="./images/icons.svg#icon-antenna-1"></use>
+                </svg>
+              </div>
+              <h3 className={s.advantagesList}>Strategy</h3>
+              <p className={s.paragraph}>
+                Our goal is to identify the business problem to walk away with
+                the perfect and creative solution.
+              </p>
+            </li>
+            <li className={s.advantagesPunkts}>
+              <div className={s.iconBox}>
+                <svg className={s.advIcon} width="64" height="64">
+                  <use href="./images/icons.svg#icon-clock-1"></use>
+                </svg>
+              </div>
+              <h3 className={s.advantagesList}>Punctuality</h3>
+              <p className={s.paragraph}>
+                Bring the key message to the brand&apos;s audience for the best
+                price within the shortest possible time.
+              </p>
+            </li>
+            <li className={s.advantagesPunkts}>
+              <div className={s.iconBox}>
+                <svg className={s.advIcon} width="64" height="64">
+                  <use href="./images/icons.svg#icon-diagram-1"></use>
+                </svg>
+              </div>
+              <h3 className={s.advantagesList}>Diligence</h3>
+              <p className={s.paragraph}>
+                Research and confirm brands that present the strongest digital
+                growth opportunities and minimize risk.
+              </p>
+            </li>
+            <li className={s.advantagesPunkts}>
+              <div className={s.iconBox}>
+                <svg className={s.advIcon} width="64" height="64">
+                  <use href="./images/icons.svg#icon-astronaut-1"></use>
+                </svg>
+              </div>
+              <h3 className={s.advantagesList}>Technologies</h3>
+              <p className={s.paragraph}>
+                Design practice focused on digital experiences. We bring forth a
+                deep passion for problem-solving.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default What;
