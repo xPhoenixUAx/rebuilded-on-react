1f96c76cf127f7dda9494e35e3aeaddf9cd401a0
xPhoenixUAx <pavloff12@gmail.com>
2024-10-11 17:56:06 +0300

    upd

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,19 @@
-import React from "react";
 import s from "./Hero.module.css";
-  const Hero = () => {
-  return <div>
-    <h1 className = {s.heroSectionTitle} </hi>
-  </div>;
+const Hero = () => {
+  return (
+    <div>
+      <section className="heroSection">
+        <div className={s.container}>
+          <h1 className={s.heroSectionTitle}>
+            Effective Solutions for Your Business
+          </h1>
+          <button type="button" className={s.buttonOrder}>
+            Order Service
+          </button>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Hero;
